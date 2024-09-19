bbcfa9a2ca454ce3138e11c41818ae7c3daf658e
Mozart Maia <mozartmaia89@gmail.com>
2024-09-19 14:26:11 -0300

    first

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,124 @@
-import Image from "next/image";
+"use client";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  const [checkboxListState, setCheckboxListState] = useState(Array(1681).fill(false));
+  const [time, setTime] = useState(0);
+  const [intervalRunning, setIntervalRunning] = useState<any>(false);
+
+  function handleChangeCheckbox(index:number) {
+    const nextCheckbox = checkboxListState.map((c:any,i:any) => {
+      if (i === index){
+        c =  !c;
+      }
+      return c;
+    });   
+    setCheckboxListState(nextCheckbox);
+  }
+
+  function updateGame(cells: any) {
+    const newCells = cells.map((c: any,i:any) => {
+      let neighbour = 0;
+      //left
+      if (cells[i-1]){
+        neighbour += 1;
+      }
+      //right
+      if (cells[i+1]){
+        neighbour += 1;
+      }
+      //bottom
+      if (cells[i+58]){
+        neighbour += 1;
+      }
+      //bottom-right
+      if (cells[i+59]){
+        neighbour += 1;
+      }
+      //bottom-left
+      if (cells[i+57]){
+        neighbour += 1;
+      }
+      //top-right
+      if (cells[i-57]){
+        neighbour +=1;
+      }
+      //top
+      if (cells[i-58]){
+        neighbour +=1;
+      }
+      //top-left
+      if (cells[i-59]){
+        neighbour +=1;
+      }
+
+      if (c) {
+        if (neighbour < 2 || neighbour > 3){
+          c = false;
+        }
+        return c 
+      } else {
+        if (neighbour === 3){
+          c = true;
+        }   
+        return c;
+      }
+      
+    })
+    setCheckboxListState(newCells);   
+  }
+
+  useEffect(() => {
+    //in this interval I will keep updating the values of checkbox grid
+    const interval = setInterval(() => {
+      if (intervalRunning){
+        updateGame(checkboxListState);
+        setTime(time + 1);
+      }        
+    }, 500);
+
+    //Clearing the interval
+    return () => clearInterval(interval);
+}, [time]);
+
+
+  function handleStartInterval(){
+    setIntervalRunning(true) 
+    setTime(time+1);   
+  }
+
+  function handleClearInterval(){
+    setIntervalRunning(false);
+    setTime(0);
+  }
+
+
+  useEffect(() => {
+    renderCheckbox();
+  },[])
+
+  function renderCheckbox() {
+    const listCheckbox = [];
+    //in resolution 1024/768 works as square (almost)
+    for(let i=0; i<=1681; i++){
+      listCheckbox.push(<input type="checkbox" key={`${i}`} id={`${i}`} checked={checkboxListState[i]} onChange={e => handleChangeCheckbox(Number(e.target.id))} />);
+          }   
+    return listCheckbox;
+  }
+
+
+  return (
+    <main className="h-screen w-full bg-slate-300">
+      <div className="justify-center">{time}</div>
+      <div className=" grid grid-cols-2">
+        <button onClick={handleStartInterval} className="border border-slate-900 rounded-md bg-green-500 hover:bg-green-400 ">Start</button>
+        <button onClick={handleClearInterval} className="border border-slate-900 rounded-md bg-yellow-500 hover:bg-yellow-400">Pause</button>
+
+      </div>
+      <div className="bg-blue-700 m-auto w-3/4 p-1">
+        { renderCheckbox() }
+      </div>      
+    </main>
   );
 }
